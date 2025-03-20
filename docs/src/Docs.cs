16ae0a444e874c03bea7b1daec9185b050667109
Marcin Celej <marcin.celej@gmail.com>
2025-03-20 16:50:34 +0100

    #10: #docs: Added `Docs` test transforming all markdowns inside the project and adding PlantUML diagrams as images

---
+++ docs/src/Docs.cs
@@ -0,0 +1,14 @@
+﻿using Synergy.Architecture.Diagrams.Markdown;
+using Xunit;
+
+namespace Synergy.Sample.Documentation;
+
+public class Docs
+{
+    [Fact(DisplayName = "Inject PlantUML diagrams into all *.md files starting from the root")]
+    public void inject_plantuml_diagrams()
+    {
+        var root = Root.Folder.Path;
+        PlantUmlDiagrams.Process(root);
+    }
+}
