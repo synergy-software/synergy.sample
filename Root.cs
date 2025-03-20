a70c7b4e3294866cd1b011ec8f77e16830689b03
Marcin Celej <marcin.celej@gmail.com>
2025-03-20 16:41:25 +0100

    #10: #docs: Added Documentation.csproj for automation of docs generation

---
+++ Root.cs
@@ -0,0 +1,18 @@
+﻿using Synergy.Documentation.Code;
+
+namespace Synergy.Sample;
+
+public static class Root
+{
+    /// <summary>
+    /// Represents the root folder of the Git repository.
+    /// This property holds the path to the current working directory on the hard drive.
+    /// </summary>
+    public static readonly CodeFolder Folder = CodeFolder.Current();
+    
+    /// <summary>
+    /// Represents the solution file for the Synergy.Framework project.
+    /// This property contains the path to the 'Synergy.Framework.sln' file located within the root folder on the hard drive.
+    /// </summary>
+    private static readonly CodeFile Solution = CodeFolder.Current().File("Synergy.Framework.sln");
+}
