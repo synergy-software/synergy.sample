48ac24c100b53d5c936b7a761cf97a9931c265c2
Marcin Celej <marcin.celej@gmail.com>
2025-05-25 07:33:21 +0200

    #7 Updated documentation in Root.cs to clarify project structure and file paths. Added WARNING to not move this file to a different location.

--- Root.cs
+++ Root.cs
@@ -2,17 +2,22 @@
 
 namespace Synergy.Sample;
 
+/// <summary>
+/// Represents the root of the Synergy.Sample solution.
+/// This class provides static properties to access the root folder and the solution file of the project.
+/// WARNING: Do not move this file to a different location, as it will break the path.
+/// </summary>
 public static class Root
 {
     /// <summary>
     /// Represents the root folder of the Git repository.
-    /// This property holds the path to the current working directory on the hard drive.
+    /// This property holds the path to the current working directory on the hard drive of this file.
     /// </summary>
     public static readonly CodeFolder Folder = CodeFolder.Current();
 
     /// <summary>
-    /// Represents the solution file for the Synergy.Framework project.
-    /// This property contains the path to the 'Synergy.Framework.sln' file located within the root folder on the hard drive.
+    /// Represents the solution file for the Synergy.Sample solution file.
+    /// This property contains the path to the 'Synergy.Sample.sln' file located within the root folder on the hard drive.
     /// </summary>
-    private static readonly CodeFile Solution = CodeFolder.Current().File("Synergy.Framework.sln");
+    private static readonly CodeFile Solution = CodeFolder.Current().File("Synergy.Sample.sln");
 }
